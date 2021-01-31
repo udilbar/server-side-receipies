f68052e8e96b692f589ad6bd97e7bc3280760c8f
Ndn1618 <ndilbar1618@gmail.com>
2021-01-31 13:20:01 +0500

    Add data for rendering into html files with ejs

---
+++ data.js
@@ -0,0 +1,187 @@
+module.exports = {
+  receipts: [
+    {
+      id: 1,
+      receiptName: "Delicious Homemade Burger",
+      receiptText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tristique nisl vitae luctus sollicitudin. Fusce consectetur sem eget dui tristique, ac posuere arcu varius.",
+      link: "/1",
+      preparationTime: 15,
+      cookTime: 30,
+      yields: 8,
+      preparationSteps: [
+        {
+          step: 1,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+        },
+        {
+          step: 2,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+        },
+        {
+          step: 3,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+        },
+        {
+          step: 4,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+        }
+      ],
+      ingredients: [
+        {
+          id: "ingredient1",
+          name: "4 Tbsp (57 gr) butter"
+        },
+        {
+          id: "ingredient2",
+          name: "2 large eggs"
+        },
+        {
+          id: "ingredient3",
+          name: "2 yogurt containers granulated sugar"
+        },
+        {
+          id: "ingredient4",
+          name: "1 vanilla or plain yogurt, 170g container"
+        },
+        {
+          id: "ingredient5",
+          name: "2 yogurt containers unbleached white flour"
+        },
+        {
+          id: "ingredient6",
+          name: "1.5 yogurt containers milk"
+        },
+        {
+          id: "ingredient7",
+          name: "1/4 tsp cinnamon"
+        },
+        {
+          id: "ingredient8",
+          name: "1 cup fresh blueberries"
+        }
+      ]
+    },
+    {
+      id: 2,
+      receiptName: "Vegetarian cheese salad",
+      receiptText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tristique nisl vitae luctus sollicitudin. Fusce consectetur sem eget dui tristique, ac posuere arcu varius.",
+      link: "/2",
+      preparationTime: 15,
+      cookTime: 30,
+      yields: 8,
+      preparationSteps: [
+        {
+          step: 1,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+        },
+        {
+          step: 2,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+        },
+        {
+          step: 3,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+        },
+        {
+          step: 4,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+        }
+      ],
+      ingredients: [
+        {
+          id: "ingredient1",
+          name: "4 Tbsp (57 gr) butter"
+        },
+        {
+          id: "ingredient2",
+          name: "2 large eggs"
+        },
+        {
+          id: "ingredient3",
+          name: "2 yogurt containers granulated sugar"
+        },
+        {
+          id: "ingredient4",
+          name: "1 vanilla or plain yogurt, 170g container"
+        },
+        {
+          id: "ingredient5",
+          name: "2 yogurt containers unbleached white flour"
+        },
+        {
+          id: "ingredient6",
+          name: "1.5 yogurt containers milk"
+        },
+        {
+          id: "ingredient7",
+          name: "1/4 tsp cinnamon"
+        },
+        {
+          id: "ingredient8",
+          name: "1 cup fresh blueberries"
+        }
+      ]
+    },
+    {
+      id: 3,
+      receiptName: "Delicious Homemade Burger",
+      receiptText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tristique nisl vitae luctus sollicitudin. Fusce consectetur sem eget dui tristique, ac posuere arcu varius.",
+      link: "/3",
+      preparationTime: 15,
+      cookTime: 30,
+      yields: 8,
+      preparationSteps: [
+        {
+          step: 1,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+        },
+        {
+          step: 2,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+        },
+        {
+          step: 3,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+        },
+        {
+          step: 4,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+        }
+      ],
+      ingredients: [
+        {
+          id: "ingredient1",
+          name: "4 Tbsp (57 gr) butter"
+        },
+        {
+          id: "ingredient2",
+          name: "2 large eggs"
+        },
+        {
+          id: "ingredient3",
+          name: "2 yogurt containers granulated sugar"
+        },
+        {
+          id: "ingredient4",
+          name: "1 vanilla or plain yogurt, 170g container"
+        },
+        {
+          id: "ingredient5",
+          name: "2 yogurt containers unbleached white flour"
+        },
+        {
+          id: "ingredient6",
+          name: "1.5 yogurt containers milk"
+        },
+        {
+          id: "ingredient7",
+          name: "1/4 tsp cinnamon"
+        },
+        {
+          id: "ingredient8",
+          name: "1 cup fresh blueberries"
+        }
+      ]
+    }
+  ]
+}
